2ac02072189933c21b45c621499502dda97e1cd8
Baron105 <76466796+Baron105@users.noreply.github.com>
2023-10-13 10:22:38 +0530

    updated Factorial_Trailing_Zeroes.java
    added an O(1) solution to [Factorial Trailing Zeroes](https://leetcode.com/problems/factorial-trailing-zeroes) using Legendre's Formula for Factorials

--- MediumDifficulty/src/Factorial_Trailing_Zeroes.java
+++ MediumDifficulty/src/Factorial_Trailing_Zeroes.java
@@ -27,8 +27,26 @@ Follow up: Could you write a solution that works in logarithmic time complexity?
 */
 
 /*
+Solution Walkthrough:
+
+The naive method of calcylating factorial and counting zeroes is inefficient as it will overflow very fast.
+
 To solve this question, we make use of the Legendre's formula which gives an expression for the exponent of the largest power of a prime p that divides the factorial n!.
 Please refer to https://en.wikipedia.org/wiki/Legendre%27s_formula to learn more about it.
 
-Now, 
+Now, we know that zeroes can be there in the factorial when 2 and 5 both are there some number of times. Therefore to get number of zeroes, we see what is the maximum power of both 2 and 5 which divides the number and take the minimum of that.
+Further we can see that max power of 2 in any factorial will be greater than or equal to max power of 5 in the same factorial. As (n/2) >= (n/5), (n/4) >= (n/25), and so on.
+So we will calculate the max power of 5 and output it as the number of trailing zeroes in the factorial.
+
+Note that we cannot apply Legendre's formula directly for 10 as it is not prime.
 */
+
+class Solution {
+    public int trailingZeroes(int n) {
+        return (n/5+n/25+n/125+n/625+n/3125);
+      // note that we have precisely done the formula as integer division is basically taking floor at each step.
+      // furthermore, we have stopped at n/3125 as n <= 10000 which means n/15625 is always zero, so further terms can be safely neglected.
+      // then we can see that we get an O(1) solution for this problem.
+      // more terms can be added in the sum if the constraints for n are changed.
+    }
+}
